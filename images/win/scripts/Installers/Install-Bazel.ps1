3431de28f17e15234430cbc13e63a2c1379c9a9d
Mikhail Timofeev <v-mitim@microsoft.com>
2020-05-19 17:41:08 +0300

    add bazel install

--- images/win/scripts/Installers/Install-Bazel.ps1
+++ images/win/scripts/Installers/Install-Bazel.ps1
@@ -3,4 +3,6 @@
 ##  Desc:  Install Bazel and Bazelisk (A user-friendly launcher for Bazel)
 ################################################################################
 
+Choco-Install -PackageName bazel
+
 npm install -g @bazel/bazelisk
