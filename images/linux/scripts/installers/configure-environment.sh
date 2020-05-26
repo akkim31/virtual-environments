4fa86b54db7ae538c9def6e8b0daceec9c246385
Mikhail Timofeev <v-mitim@microsoft.com>
2020-05-26 15:02:35 +0300

    change waagent

--- images/linux/scripts/installers/configure-environment.sh
+++ images/linux/scripts/installers/configure-environment.sh
@@ -5,3 +5,12 @@ echo ImageOS=$IMAGE_OS | tee -a /etc/environment
 # This directory is supposed to be created in $HOME and owned by user(https://github.com/actions/virtual-environments/issues/491)
 mkdir -p /etc/skel/.config/configstore
 echo 'export XDG_CONFIG_HOME=$HOME/.config' | tee -a /etc/skel/.bashrc
+
+# Set /mnt as swap
+echo "cat /etc/waagent.conf before the changes"
+cat /etc/waagent.conf
+sed -i 's/ResourceDisk.Format=n/ResourceDisk.Format=y/g'
+sed -i 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g'
+sed -i 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=4096/g'
+echo "cat /etc/waagent.conf after the changes"
+cat /etc/waagent.conf
