b5376669ce44b14d696b5da9bd56f51a196f6461
Mikhail Timofeev <v-mitim@microsoft.com>
2020-10-02 14:31:49 +0300

    check postgresql service status

--- images/macos/provision/core/postgresql.sh
+++ images/macos/provision/core/postgresql.sh
@@ -6,6 +6,21 @@ brew install postgres
 #Service postgresql should be started before use.
 brew services start postgresql
 
+echo "Check PostgreSQL service is running"
+i=10
+COMMAND='brew services list | grep "postgresql started"'
+while [ $i -gt 0 ]; do
+    echo "Check PostgreSQL service status"
+    eval $COMMAND && break
+    ((i--))
+    if [ $i == 0 ]; then
+        echo "PostgreSQL service not started, all attempts exhausted"
+        exit 1
+    fi
+    echo "PostgreSQL service not started, wait 10 more sec, attempts left: $i"
+    sleep 10
+done
+
 #Verify that PostgreSQL is ready for accept incoming connections.
 # exit codes:
 # ready - 0
