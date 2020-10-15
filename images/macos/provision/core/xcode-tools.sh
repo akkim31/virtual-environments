e74d223cbfd60e6502c4c346731541566a0be131
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2020-10-15 11:40:21 +0300

    workaround for 12.0.1 version

--- images/macos/provision/core/xcode-tools.sh
+++ images/macos/provision/core/xcode-tools.sh
@@ -52,6 +52,11 @@ do
     VERSION_SPECIFIC_FOLDERS=$(ls "${HOME}/Library/Caches/" | grep Xcode)
     echo "get version specific root folder files = ${VERSION_SPECIFIC_FOLDERS} for ${XCODE_VERSION}"
 
+    # hack for 12.0.1 version
+    if [[ $XCODE_VERSION == "12" ]] && is_Catalina ; then
+        XCODE_VERSION="12.0.1"
+    fi
+
     # hack for beta
     if [[ $XCODE_VERSION == *"beta"* ]] && is_Catalina ; then
         find "${HOME}/Library/Caches/XcodeInstall/" -name "Xcode_${XCODE_VERSION}.xip" -o -name "Xcode_${XCODE_VERSION}_*.xip" -type f -exec mv -f {} "${WORK_DIR}" \;
