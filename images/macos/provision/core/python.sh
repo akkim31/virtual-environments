0d91c65eaed38c4980a38517a638dfee8c68b927
Mikhail Timofeev <v-mitim@microsoft.com>
2020-12-07 18:37:00 +0300

    add python2 official

--- images/macos/provision/core/python.sh
+++ images/macos/provision/core/python.sh
@@ -7,22 +7,11 @@ echo "Installing Python Tooling"
 echo "Brew Installing Python 3"
 brew install python@3.9
 
-echo "Install pip for Python 2"
-# Set permissions to install pip modules in the main packages directory
-sudo chmod -R 777 /Library/Python/2.7
-sudo chmod -R 777 /System/Library/Frameworks/Python.framework/Versions/2.7
-# Create symlink for backwards compatibility
-ln -s /usr/bin/python /usr/local/bin/python
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-# Workaround for High Sierra pip, since it installs into /usr/bin. On the modern macOSs pip installation directory is /usr/local/bin
-if ! is_HighSierra; then
-    python get-pip.py
-else
-    sudo chmod 777 /usr/bin
-    python get-pip.py
-    ln -s /usr/bin/pip /usr/local/bin/pip
-    sudo chmod 755 /usr/bin
-fi
+echo "Install latest Python 2"
+Python2Url="https://www.python.org/ftp/python/2.7.18/python-2.7.18-macosx10.9.pkg"
+download_with_retries $Python2Url "/tmp" "python2.pkg"
+sudo installer -pkg /tmp/python2.pkg -target /
+pip install --upgrade pip
 
 echo "Installing pipx"
 export PIPX_BIN_DIR=/usr/local/opt/pipx_bin
